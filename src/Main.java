748742b7d145d4bc1b07a67808aee4ea87fda82f
KoshkaName <114123386+KoshkaName@users.noreply.github.com>
2022-10-10 13:01:00 +1000

    Main added

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static String[] products = { "Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка" };
-    public static int[] prices = { 50, 135, 65, 53 };
+    public static String[] products = {"Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка"};
+    public static int[] prices = {50, 135, 65, 53};
     public static int MIN_COST_FOR_BONUS = 1000;
 
     // В стоимости этих товаров каждые три товара должны стоить как два:
-    public static String[] productsOnSale = { "Хлеб", "Мороженка" };
+    public static String[] productsOnSale = {"Хлеб", "Мороженка"};
 
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в магазин!");
@@ -39,16 +39,16 @@ public class Main {
         System.out.println("Ваша корзина покупок:");
         int sum = 0;
         for (int i = 0; i < products.length; i++) {
-            sum += prices[i] * counts[i];
+
         }
         boolean doBonus = sum >= MIN_COST_FOR_BONUS;
         for (int i = 0; i < products.length; i++) {
             if (counts[i] != 0) {
+
                 boolean isOnSale = false;
                 for (String saleProduct : productsOnSale) {
                     if (products[i].equals(saleProduct)) {
                         isOnSale = true;
-                        break;
                     }
                 }
 
